3a1fc613d68f37e81b208816af4494206ae56832
ZODIAC <melenteevvadim@gmail.com>
2023-08-06 23:49:19 +0300

    VS fix

--- client/desktop/windows/Form1.cs
+++ client/desktop/windows/Form1.cs
@@ -10,11 +10,36 @@ using System.Windows.Forms;
 
 namespace GigaChat
 {
-    public partial class Form1 : Form
+    public partial class winRegister : Form
     {
-        public Form1()
+        public winRegister()
         {
             InitializeComponent();
+        }
+
+        private void exitReg_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void exitReg_MouseHover(object sender, EventArgs e)
+        {
+            exitReg.BackColor = Color.FromArgb(63,72,204);
+        }
+
+        private void exitReg_MouseLeave(object sender, EventArgs e)
+        {
+            exitReg.BackColor = Color.FromArgb(0, 162, 232); ;
+        }
+
+        private void passwordVisCheckBox_Click(object sender, EventArgs e)
+        {
+            passwordBoxReg.UseSystemPasswordChar = (!passwordBoxReg.UseSystemPasswordChar) ? true : false;
+        }
+
+        private void registerReg_Click(object sender, EventArgs e)
+        {
+            this.Hide();
         }
     }
 }
